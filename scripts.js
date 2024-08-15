b019cd00603cec8c4ae3dc0d2deba600ff552ffc
ReSoaresOFC <132530719+ReSoaresOFC@users.noreply.github.com>
2024-08-15 17:19:15 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event){
-    event.preventDefault(); // Evita o envio real do formulário
-    
-    // Exibe a mensagem de sucesso
-    document.getElementById("successMessage").style.display = "block";
-    
-    // Limpa o formulário
-    document.getElementById("contactForm").reset();
-});
